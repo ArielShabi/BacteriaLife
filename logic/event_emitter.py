bd40b835bc4dcf32d8747e064b03d25b94f5a4c5
ArielShabi <4ariel6@gmail.com>
2024-03-10 22:30:59 +0200

    Add EventEmitter class for event handling

---
+++ logic/event_emitter.py
@@ -0,0 +1,17 @@
+from typing import Callable
+from collections import defaultdict
+
+
+class EventEmitter:
+    def __init__(self):
+        self.listeners: dict[str, list[Callable]] = defaultdict(list)
+
+    def add_listener(self, event_name: str, listener: Callable):
+        self.listeners[event_name].append(listener)
+
+    def remove_listener(self, event_name: str, listener: Callable):
+        self.listeners[event_name].remove(listener)
+
+    def fire_event(self, event_name: str, data):
+        for listener in self.listeners[event_name]:
+            listener(data)
