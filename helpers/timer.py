c350497c062ed9e87d42377e6c053b6182709cae
ArielShabi <4ariel6@gmail.com>
2024-03-10 22:30:52 +0200

    Add Timer class

---
+++ helpers/timer.py
@@ -0,0 +1,22 @@
+from PyQt5.QtCore import QTimer, QObject, pyqtSignal
+
+
+class Timer(QObject):
+    timeout = pyqtSignal()
+
+    def __init__(self, interval=None):
+        super().__init__()
+        self.timer: QTimer = QTimer(self)
+        self.timer.timeout.connect(self.timeout.emit)
+        self.interval = interval
+
+    def start(self, interval=None):
+        if interval:
+            self.interval = interval
+        self.timer.start(self.interval)
+
+    def stop(self):
+        self.timer.stop()
+
+    def is_alive(self):
+        return self.timer.isActive()
