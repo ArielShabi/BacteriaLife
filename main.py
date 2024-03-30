dcc9e8b23563ceb443d7a49a8f8430e1d5c6b058
ArielShabi <4ariel6@gmail.com>
2024-03-30 16:11:43 +0300

    add help

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ from PyQt5.QtWidgets import QApplication
 
 from ui.main_window import MainWindow
 
+def show_help():
+    print("Usage: python main.py")
+    print("Runs the bacteria life simulation GUI.")
+    print("Enjoy :)")
+
+
 if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1] == "--help":
+        show_help()
+        sys.exit(0)
+    
+    
     app = QApplication(sys.argv)
 
     font = app.font()
@@ -15,3 +26,5 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
+
